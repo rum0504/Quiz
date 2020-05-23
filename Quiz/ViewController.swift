9213ba60b3432afcb26c3587b1f479e3b1697bde
Ma <manakamm4062@gmail.com>
2020-05-23 16:17:08 +0900

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by 齊藤愛華 on 2020/05/23.
+//  Copyright © 2020 com.lifeistech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
